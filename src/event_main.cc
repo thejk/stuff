8c70d22a2e0cabbfc007d4b06a0ebec797ee7d0f
Joel Klinghed <the_jk@spawned.biz>
2026-02-13 11:02:32 +0100

    Remove leftovers

--- src/event_main.cc
+++ src/event_main.cc
@@ -17,18 +17,6 @@
 #include "http.hh"
 #include "sender_client.hh"
 
-    /*
-token=gIkuvaNzQIHg97ATvDxqgjtO
-team_id=T0001
-team_domain=example
-channel_id=C2147483705
-channel_name=test
-user_id=U2147483697
-user_name=Steve
-command=/weather
-text=94070
-        */
-
 using namespace stuff;
 
 namespace {
